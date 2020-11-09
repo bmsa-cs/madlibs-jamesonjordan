555eade79f9b71e5fba47c6bbe0d0373128de653
Jameson Jordan <jjordan24@biomedscienceacademy.org>
2020-11-09 19:41:53 +0000

    Added most of my inputs and started most of the story, almost all the way done

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+"""
+MadLibs
+Author: Jameson Jordan
+Period/Core: Core 3
 
-print("Welcome to Jameson's Mad Lib, get ready for a fun story! "")
 
+"""
+
+watersilde_1 = input("Enter a name for a waterslide: ")
+workername_1 = input("Enter a name: ")
+verb_1 = input("Enter a verb, that does not end in ing: ")
+color_color = input("Enter a color: ")
+watersilde_2 = input("Enter another name for a waterslide: ")
+dippin_dots = input("Enter a flavor of ice cream: ")
+animal_1 = input("Enter a animal, preferably a mammal: ")
+name_2 = input("Enter another name: ")
+
+
+print(f"I woke up in the hotel of Kalahari, ready to go down to the waterpark for the day. \n I enter the waterpark gazing at all of the slides, {watersilde_1} looks very intresting. \n")
+print(f"I get to the top of the slide and see the worker {workername_1} standing by the slide. \n He calls me over and says I can go down, I get to the bottom and get out of the slide and {verb_1} to the next slide. \n ")
+print(f"I see a bright {color_color} slide and I decide I want to go to that one next.\n I then figured out the slide was called {watersilde_2}, I thought it was a pretty cool name. \n ")
+print(f"After riding a couple more slides \n I decide I want to get some Dippin' Dots \n I decided on the flavor {dippin_dots}. \n They ended up being very delcious. ")
+print(f"All of a sudden I see everyone at the waterpark running towards the doors, and I was wondering why. Then I realize a wild {animal_1} had somehow got into the waterpark. \n {name_2} who was from animal control comes chasing after the wild animal. \n Somehow he ends up catching him just in time. ")
